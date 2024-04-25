c0f232e6d6585f698fda4acaa796390bbf525689
Ark Grayer (Igor) <igorlavras@hotmail.com>
2024-04-25 15:28:21 -0300

    Player Character Controller ✨💡
    Finished the controller movement for Player

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -1,31 +1,77 @@
 // Copyright NovaDev
+// Este código define a implementação das funções do controlador de jogador para o jogo "Aura".
+// O controlador do jogador é responsável por processar as entradas do jogador e controlar o comportamento do jogador no jogo.
 
 
 #include "Player/AuraPlayerController.h"
 #include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
 
+// Construtor padrão da classe AAuraPlayerController.
 AAuraPlayerController::AAuraPlayerController() {
-
+	
+	// Define que este controlador será replicado para os clientes.
 	bReplicates = true;
 	
 }
 
+// Função chamada quando o jogo começa.
 void AAuraPlayerController::BeginPlay() {
 	Super::BeginPlay();
+	// Garante que o contexto do Aura esteja presente.
 	check(AuraContext);
 
+	// Obtém o subsistema local de entrada aprimorada para o jogador atual e checa para caso não passe no teste, dê um
+	//crash
 	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
 	check(Subsystem);
+	// Adiciona o contexto do Aura ao subsistema de mapeamento de entrada.
 	Subsystem->AddMappingContext(AuraContext, 0);
 
-	bShowMouseCursor = true;
-	DefaultMouseCursor = EMouseCursor::Default;
-
+	// Configuração do modo de entrada para o jogo e UI.
 	FInputModeGameAndUI InputModeData;
 	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
 	InputModeData.SetHideCursorDuringCapture(false);
 	SetInputMode(InputModeData);
+
+	// Habilita a exibição do cursor do mouse e define o cursor padrão.
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
 	
+}
+
+void AAuraPlayerController::SetupInputComponent() {
+	Super::SetupInputComponent();
+
+	// Obtém o componente de entrada aprimorada e o vincula a ação de movimento e ao mesmo tempo que obtém o componente
+	//faz uma checagem de teste para caso o Input não funcione, dê um crash para saber que o problema foi aqui.
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AAuraPlayerController::Move);
+	
+}
+
+// Função para mover o jogador com base na entrada do jogador.
+void AAuraPlayerController::Move(const FInputActionValue& InputActionValue) {
+
+	// Obtém o vetor de entrada do eixo X e Y.
+	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
+	// Obtém a rotação de controle atual.
+	const FRotator Rotation = GetControlRotation();
+	const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+
+	// Obtém as direções para frente e para a direita com base na rotação do controle.
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	// Verifica se o controlador possui um pawn controlado.
+	if (APawn* ControlledPawn= GetPawn<APawn>()) {
+
+		// Adiciona entrada de movimento para frente e para trás com base no vetor de entrada do eixo Y.
+		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
+		// Adiciona entrada de movimento para a direita e esquerda com base no vetor de entrada do eixo X.
+		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+		
+	}
 }
 
 
--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -1,4 +1,6 @@
 // Copyright NovaDev
+// Este código define a classe do controlador de jogador para o jogo "Aura".
+// O controlador do jogador é responsável por processar as entradas do jogador e controlar o comportamento do jogador no jogo.
 
 #pragma once
 
@@ -6,12 +8,12 @@
 #include "GameFramework/PlayerController.h"
 #include "AuraPlayerController.generated.h"
 
-
+// Declaração prévia das classes.
 class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
 
-/**
- * 
- */
+// Classe do controlador de jogador.
 UCLASS()
 class AURA_API AAuraPlayerController : public APlayerController
 {
@@ -22,9 +24,18 @@ public:
 	
 protected:
 	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
 
 private:
+	// Contexto de mapeamento de entrada para o Aura.
 	UPROPERTY(EditAnywhere, Category="Input")
 	TObjectPtr<UInputMappingContext> AuraContext;
+
+	// Ação de movimento.
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UInputAction> MoveAction;
+
+	// Função para mover o jogador.
+	void Move(const FInputActionValue& InputActionValue);
 	
 };
