04855e2f2c174854ef4be632ba7316a3fa35b013
Ark Grayer (Igor) <igorlavras@hotmail.com>
2024-06-12 10:13:50 -0300

    Create Health and Mana attributes for AttributeSet ✨🚧💡

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -2,4 +2,54 @@
 
 
 #include "AbilitySystem/AuraAttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
 
+UAuraAttributeSet::UAuraAttributeSet() {
+	
+}
+
+//Registering attributes to be replicated from server to client and update and set those values
+
+void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const {
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, Mana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+	
+}
+
+
+//Generate a rep on server-side for Health attribute
+
+void UAuraAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const {
+
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, Health, OldHealth);
+	
+}
+
+//Generate a rep on server-side for MaxHealth attribute
+
+void UAuraAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const {
+
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, MaxHealth, OldMaxHealth);
+	
+}
+
+//Generate a rep on server-side for Mana attribute
+
+void UAuraAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const {
+
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, Mana, OldMana);
+	
+}
+
+//Generate a rep on server-side for MaxMana attribute
+
+void UAuraAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const {
+	
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, MaxMana, OldMaxMana);
+	
+}
--- Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -13,5 +13,58 @@ UCLASS()
 class AURA_API UAuraAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+
+public:
+	//Start the constructor for AttributeSet
+	UAuraAttributeSet();
+	//Create a function to rep server and client values (or attributes)
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	/**
+	* Property for MaxHealth can be read on blueprints, it's replicated using the function OnRep_Health when using
+	* it and categorized on "Vital Attributes"
+	**/
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Vital Attributes")
+	FGameplayAttributeData Health;
+
+	/**
+	* Property for MaxHealth can be read on blueprints, it's replicated using the function OnRep_MaxHealth when using
+	* it and categorized on "Vital Attributes"
+	**/
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category = "Vital Attributes")
+	FGameplayAttributeData MaxHealth;
+
+	/**
+	* Property for MaxHealth can be read on blueprints, it's replicated using the function OnRep_Mana when using
+	* it and categorized on "Vital Attributes"
+	**/
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Mana, Category = "Vital Attributes")
+	FGameplayAttributeData Mana;
+
+	/**
+	* Property for MaxHealth can be read on blueprints, it's replicated using the function OnRep_MaxMana when using
+	* it and categorized on "Vital Attributes"
+	**/
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxMana, Category "Vital Attributes")
+	FGameplayAttributeData MaxMana;
+
+	//Function to use the old health value to check if it's valid
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+
+	//Function to use the old MaxHealth value to check if it's valid
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+
+	//Function to use the old MaxHealth value to check if it's valid
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+
+	//Function to use the old Max_Mana value to check if it's valid
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
+
+	
+	
 	
 };
